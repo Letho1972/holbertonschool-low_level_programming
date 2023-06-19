da2fc47a7b85c76d4d6b428447784652ca2aec49
LETHO1972 <ericmarescq@gmail.com>
2023-06-19 02:20:48 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+* main - Programming is like building a multilingual puzzle
+* Return: 0
+*/
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
