74de54d82557e645be9ba06055a5262c3117fba1
LETHO1972 <ericmarescq@gmail.com>
2023-06-20 04:45:01 -0700

    Print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - positif or negatif
+ * Return: success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 97; letter <= 122; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
