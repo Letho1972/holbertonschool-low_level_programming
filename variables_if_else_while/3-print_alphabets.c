42386ca5878caf5da2bd32876007fe93958c3246
LETHO1972 <ericmarescq@gmail.com>
2023-06-20 05:12:11 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - positif or negatif
+ * Return: success
+ */
+
+int main(void)
+{
+	char letter;
+
+	char LETTER;
+
+	for (letter = 97; letter <= 122; letter++)
+	{
+		putchar(letter);
+	}
+
+	for (LETTER = 65; LETTER <= 90; LETTER++)
+	{
+		putchar(LETTER);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
