1a3278e8caf2c6adc5904661e7c59d1464d408ec
LETHO1972 <ericmarescq@gmail.com>
2023-06-21 03:19:11 -0700

    Patience, persistence

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,15 +13,13 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
+	
+	if (i != 57)
+	{		putchar(',');
+			putchar(' ');
 	}
-	{
-		if (i != 57)
-			putchar(',');
-		putchar(' ');
-	}
-	{
+	
 
-		putchar('\n');
 	}
 	return (0);
 }
