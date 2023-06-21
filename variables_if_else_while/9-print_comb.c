a815906286c40d123a496ed00e48fd92fa8bc070
LETHO1972 <ericmarescq@gmail.com>
2023-06-21 03:05:18 -0700

    Patience, persistence

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - positif or negatif
+ * Return: success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	{
+		if (i != 57)
+			putchar(',');
+		putchar(' ');
+	}
+	{
+
+		putchar('\n');
+	}
+	return (0);
+}
