22c8b0438697def37e449ba303afef5b12205d8d
LETHO1972 <ericmarescq@gmail.com>
2023-06-21 01:47:57 -0700

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - positif or negatif
+ * Return: success
+ */
+
+int main(void)
+{
+	int i;
+
+	int letter;
+
+		for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+		for (letter = 97; letter <= 102; letter++)
+	{
+		putchar(letter);
+	}
+
+		putchar('\n');
+
+return (0);
+}
