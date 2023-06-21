4f1dcd547fb5cbd0a4bf49d66314d6721d1e9688
LETHO1972 <ericmarescq@gmail.com>
2023-06-21 01:21:50 -0700

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - positif or negatif
+ * Return: success
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter >= 97; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+return (0);
+}
