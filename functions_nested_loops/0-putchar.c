937fe21a536d9631e723296837b307c0cc55b210
LETHO1972 <ericmarescq@gmail.com>
2023-06-22 00:46:34 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - putchar
+ * Description: print putchar
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+
+	return (0);
+}
