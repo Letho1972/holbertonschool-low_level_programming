d01323a1998b97fe3ba6a10e56769a5036615707
LETHO1972 <ericmarescq@gmail.com>
2023-06-22 02:09:59 -0700

    insomnia az

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int alphab;
+
+	for (alphab = 'a'; alphab <= 'z'; alphab++)
+	{
+		_putchar(alphab);
+	}
+	_putchar('\n');
+
+}
