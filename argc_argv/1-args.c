20b48f73f68a9bb20ec72121ff87ab2ff3e9f2aa
LETHO1972 <ericmarescq@gmail.com>
2023-07-07 05:43:29 -0700

    Write a program that prints the number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the number of arguments
+ * @argc: The number of command line arguments
+ * @argv: An array of size argc:x
+ * Return: new line
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+		{
+		printf("%d\n", argc - 1);
+
+		return (0);
+		}
