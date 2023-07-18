c5f495d12b099b230d1155628fff4b089cc402a2
LETHO1972 <ericmarescq@gmail.com>
2023-07-18 03:05:09 -0700

    Write a program that prints the name of the file

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that prints the name
+ * Return: always zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
