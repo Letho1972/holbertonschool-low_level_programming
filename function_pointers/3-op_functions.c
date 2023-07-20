e3330b668c8eb7b5b8541e22feca4ae774f99cd6
LETHO1972 <ericmarescq@gmail.com>
2023-07-20 06:27:22 -0700

    Write a program that performs simple operations

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
-
+/**
+ * op_add - addition
+ * @a: value num1 
+ * @b: value num2
+ */
 int op_add(int a, int b)
 	{
 		return (a + b);
 	}
+/**
+ * op_sub - soustraction
+ * @a: value num1
+ * @b: value num2
+ */
 int op_sub(int a, int b)
 	{
 		return (a - b);
 	}
+/**
+ * op_mul - multiplication
+ * @a: value num1
+ * @b: value num2
+ */
 int op_mul(int a, int b)
 	{
 		return (a * b);
 	}
+/**
+ * op_div - division
+ * @a: value num1
+ * @b: value num2
+ */
 int op_div(int a, int b)
 	{
 		return (a / b);
 	}
+/**
+ * op_mod - modulo
+ * @a: value num1
+ * @b: value num2
+ */
 int op_mod(int a, int b)
 	{
 		return (a % b);
