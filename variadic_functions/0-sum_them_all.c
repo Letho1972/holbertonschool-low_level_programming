474704a504863015f6308972bd71efe23580886c
LETHO1972 <ericmarescq@gmail.com>
2023-07-21 01:23:49 -0700

    Write a function that returns the sum of all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - Write a function that returns the sum of all
+ * @n: all parameters
+ * Return: Always nothing
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start(ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+	sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
