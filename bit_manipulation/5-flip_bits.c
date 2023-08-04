6abe2e0e9a4f983b5e883b77ecb9476e6c854bce
LETHO1972 <ericmarescq@gmail.com>
2023-08-04 06:46:24 -0700

    returns the number of bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ *@n: number
+ *@m: other number
+ * Return: Always 0.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+	unsigned long int xor_result = n ^ m;
+	
+	return (xor_result);
+}
